089d27f50a1f9a2f57180b7daa4ce408495c8c83
Laxynium <grzesiu15432@gmail.com>
2023-06-03 12:25:41 +0200

    Init

---
+++ ConfigurationTemplates/TemplatesFinder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationTemplates;
+
+internal static class TemplatesFinder
+{
+    internal static ReadOnlyCollection<IConfigurationSection> FindTemplates(
+        IConfigurationRoot configuration,
+        Func<string, bool> matchKey)
+    {
+        var flatten = TreeWalker.Flatten(configuration);
+        var templates = flatten
+            .Where(x => matchKey(x.Key))
+            .ToList().AsReadOnly();
+        return templates;
+    }
+}
