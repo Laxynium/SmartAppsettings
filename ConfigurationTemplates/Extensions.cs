81cf1ac7a101c34bb7cf0d7330a6062321dfc289
Laxynium <grzesiu15432@gmail.com>
2023-06-03 12:26:38 +0200

    Add poc without tests

---
+++ ConfigurationTemplates/Extensions.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationTemplates;
+
+public static partial class Extensions
+{
+    private const string TemplateMarkerPrefix = "$.";
+
+    public static void AddTemplatesSources(this ConfigurationManager configuration)
+    {
+        var templates = TemplatesFinder.FindTemplates(configuration, x => x.StartsWith(TemplateMarkerPrefix));
+
+        var result = CreateInMemoryCollection(configuration, templates);
+
+        configuration.AddInMemoryCollection(result!);
+    }
+
+    private static Dictionary<string, string> CreateInMemoryCollection(IConfiguration configuration,
+        ReadOnlyCollection<IConfigurationSection> templates)
+    {
+        var result = new Dictionary<string, string>();
+        foreach (var template in templates)
+        {
+            var key = BuildKey(template);
+            var value = BuildValue(configuration, template);
+            result.Add(key, value);
+        }
+
+        return result;
+    }
+
+    private static string BuildKey(IConfigurationSection template)
+    {
+        return template.Path.Replace(template.Key, template.Key[TemplateMarkerPrefix.Length..]);
+    }
+
+    private static string BuildValue(IConfiguration configuration, IConfigurationSection template)
+    {
+        var templateValue = template.Value ?? throw new InvalidCastException("This exception should never be thrown.");
+        var result = TemplateRegex().Replace(templateValue, match =>
+        {
+            var sectionPath = match.Groups[1].Value;
+            var section = configuration.GetSection(sectionPath);
+            var value = section.Value;
+            if (value is null)
+            {
+                if (section.GetChildren().Any())
+                {
+                    throw new InvalidOperationException(
+                        $"Configuration reference '{sectionPath}' in template '{template.Path}' is pointing to a primitive type");
+                }
+
+                throw new InvalidOperationException(
+                    $"Found a template '{template.Path}' which is referencing not existing configuration section '{sectionPath}'");
+            }
+
+            return value;
+        });
+        return result;
+    }
+
+    [GeneratedRegex("\\{([^\\}]+)\\}")]
+    private static partial Regex TemplateRegex();
+}
--- ConfigurationTemplates/TemplatesFinder.cs
+++ ConfigurationTemplates/TemplatesFinder.cs
@@ -9,10 +9,50 @@ internal static class TemplatesFinder
         IConfigurationRoot configuration,
         Func<string, bool> matchKey)
     {
-        var flatten = TreeWalker.Flatten(configuration);
+        var flatten = TreeWalker.IterativeFlatten(configuration);
         var templates = flatten
             .Where(x => matchKey(x.Key))
             .ToList().AsReadOnly();
         return templates;
+    }
+
+    private static class TreeWalker
+    {
+        public static IReadOnlyList<IConfigurationSection> IterativeFlatten(IConfigurationRoot root)
+        {
+            var result = new List<IConfigurationSection>();
+            var queue = new Queue<IConfigurationSection>(root.GetChildren());
+            while (queue.TryDequeue(out var config))
+            {
+                result.Add(config);
+                foreach (var child in config.GetChildren())
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+
+        public static IReadOnlyList<IConfigurationSection> RecursiveFlatten(IConfigurationRoot root)
+        {
+            var result = new List<IConfigurationSection>();
+            foreach (var c in root.GetChildren())
+            {
+                Walk(c, result);
+            }
+
+            return result;
+        }
+
+        private static void Walk(IConfigurationSection root, ICollection<IConfigurationSection> result)
+        {
+            result.Add(root);
+            var children = root.GetChildren();
+            foreach (var c in children)
+            {
+                Walk(c, result);
+            }
+        }
     }
 }
